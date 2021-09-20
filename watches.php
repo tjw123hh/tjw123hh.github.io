4cd5a9f26d87ae708b3bed802de3f163c2d0fe5e
Jiawei Tang <3245813583@qq.com>
2021-09-20 09:54:05 +0800

    good

--- watches.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    $_SESSION["ip"]='<script>document.writeln(returnCitySN["cip"]);</script>';
-    $_SESSION["watches"]=_SESSION["watches"]+1;
-    echo 'document.write("浏览量 {$_SESSION["watches"]}")';
-?>
